acdf8dc6e1b59233d6b8f6aa0ecde7db92763368
cenojava <cenoapple@gmail.com>
2016-10-26 19:24:03 -0500

    Add files via upload
    new file

---
+++ Demo.java
@@ -0,0 +1,11 @@
+package funfun;
+
+public class Demo {
+	//put code herefsdfsf
+	int a = 3;
+	static String robot = "robot is hungry";
+	
+	public static void main(String args[]){
+		System.out.println(robot);
+	}
+}
