4cf0f53fef4e67b32a01dce87713328f9a0ee640
Ben Mildenberger <cenoapple@gmail.com>
2016-11-09 18:41:37 -0600

    deleted necessary files

--- Demo.java
+++
@@ -1,8 +0,0 @@
-package funfun;
-
-public class Demo {
-	//put code here
-	int a = 3;
-	int b = 5;
-	
-}
--- DemoVidoeo.java
+++
@@ -1,8 +0,0 @@
-public class DemoVidoeo{
-	public static void main(String args[]){
-		int a = 3;
-		int b = 67;
-		boolean pistonState = true;
-		
-	}
-}
